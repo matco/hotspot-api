35c9ce7cf5ce9063b921c6e67c840227786b104e
Matthieu Corageoud <matthieu.corageoud@gmail.com>
2018-05-01 21:53:07 +0200

    Update database access layer: -update MySQL driver -replace C3P0 by Hikari

--- src/test/java/name/matco/hotspot/services/datasource/mocks/ConnectionProviderMock.java
+++ src/test/java/name/matco/hotspot/services/datasource/mocks/ConnectionProviderMock.java
@@ -1,29 +1,27 @@
 package name.matco.hotspot.services.datasource.mocks;
 
-import java.beans.PropertyVetoException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.UUID;
 
-import com.mchange.v2.c3p0.ComboPooledDataSource;
+import com.zaxxer.hikari.HikariDataSource;
 
 import name.matco.hotspot.services.datasource.ConnectionProvider;
 
 public class ConnectionProviderMock implements ConnectionProvider {
 
-	private ComboPooledDataSource cpds;
+	private HikariDataSource ds;
 
-	public ConnectionProviderMock() throws PropertyVetoException {
-		cpds = new ComboPooledDataSource();
-		cpds.setDriverClass("org.h2.Driver");
+	public ConnectionProviderMock() {
+		ds = new HikariDataSource();
 		//generate unique database name for each instance
 		final UUID uuid = UUID.randomUUID();
-		cpds.setJdbcUrl(String.format("jdbc:h2:mem:test%s;MODE=MYSQL", uuid));
+		ds.setJdbcUrl(String.format("jdbc:h2:mem:test%s;MODE=MYSQL", uuid));
 	}
 
 	@Override
 	public Connection getConnection() throws SQLException {
-		return cpds.getConnection();
+		return ds.getConnection();
 	}
 
 }
