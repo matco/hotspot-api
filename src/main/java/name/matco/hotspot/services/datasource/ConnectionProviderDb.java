848e736374e15f21604cc94f2df486c5f47dfe5c
Matthieu Corageoud <matthieu.corageoud@gmail.com>
2024-06-24 22:13:24 +0200

    Use MariaDB instead of MySQL JDBC driver

--- src/main/java/name/matco/hotspot/services/datasource/ConnectionProviderDb.java
+++ src/main/java/name/matco/hotspot/services/datasource/ConnectionProviderDb.java
@@ -15,7 +15,7 @@ public class ConnectionProviderDb implements ConnectionProvider {
 	public ConnectionProviderDb() {
 		final Properties properties = App.getAppProperties();
 		ds = new HikariDataSource();
-		ds.setJdbcUrl(String.format("jdbc:mysql://%s:%s/%s?characterEncoding=utf-8&useUnicode=true&useLegacyDatetimeCode=false&serverTimezone=GMT&useSSL=false", properties.getProperty("db.host"), properties.getProperty("db.port"), properties.getProperty("db.name")));
+		ds.setJdbcUrl(String.format("jdbc:mariadb://%s:%s/%s?characterEncoding=utf-8&useUnicode=true&useLegacyDatetimeCode=false&serverTimezone=GMT&useSSL=false", properties.getProperty("db.host"), properties.getProperty("db.port"), properties.getProperty("db.name")));
 		ds.setUsername(properties.getProperty("db.user"));
 		ds.setPassword(properties.getProperty("db.password"));
 	}
